4df27f8c60f77aff4600208544c8ace65531bb6e
MustansirJukker <mustanjukker@gmail.com>
2026-04-02 00:43:47 +0530

    Added connection

--- EFCoreOperatinsApp/EFCoreOperatinsApp/Program.cs
+++ EFCoreOperatinsApp/EFCoreOperatinsApp/Program.cs
@@ -1,5 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
+using EFCoreOperatinsApp.Data;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))
+);
 // Add services to the container.
 
 builder.Services.AddControllers();
